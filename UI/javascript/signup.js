6ea54c90287b27a0257b15b2699931d7756248c5
e-ian <e-ian>
2019-02-06 18:15:09 +0300

    modifies js of the signup for redirection

--- UI/javascript/signup.js
+++ UI/javascript/signup.js
@@ -7,7 +7,7 @@ function Signup() {
         email: document.getElementById("email").value,
         role: document.getElementById("role").value
     }
-
+    
     fetch('http://127.0.0.1:5000/api/v1/auth/signup', {
         method: 'POST',
         body: JSON.stringify(register),
