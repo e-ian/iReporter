6f4a912dab83822710390e69b181089b9c3aa1e9
e-ian <e-ian>
2019-02-11 14:52:53 +0300

    adds get intervention by id using javascript [Delivers#163862127]

--- UI/javascript/interventions.js
+++ UI/javascript/interventions.js
@@ -39,3 +39,46 @@ function getInterventions() {
         });
 
 }
+
+document.getElementById('getSpecific').onclick = function getSpecificIntervention(){
+    let intervention_id = document.getElementById('incidentSearch').value;
+    console.log(intervention_id)
+    let specInterventionsURL = `http://127.0.0.1:5000/api/v1/interventions/${intervention_id}`;
+    console.log(specInterventionsURL)
+    token = localStorage.getItem('access_token')
+    fetch(specInterventionsURL, {
+        headers: {
+            'Content_type': 'application/json',
+            'Authorization': `${token}`
+        }
+    })
+    
+    .then(res => res.json())
+    .then(response => {
+        console.log(response)
+        if (response.message === 'Intervention record not found'){
+            alert(response.message);} 
+        else{
+            let output = ''            
+                output += `
+                <div class="Create-record">
+                    <form id="IntForm" method="PUT">
+                    <h2>Intervention: ${intervention_id}</h2>
+                    <p>comment: ${response['intervention'].comment}</p>
+                    <p>created_by: ${response['intervention'].created_by}</p>
+                    <p>created_on: ${response['intervention'].created_on}</p>
+                    <p>image: ${response['intervention'].image}</p>
+                    <p>incident_type: ${response['intervention'].incident_type}</p>
+                    <p>location: ${response['intervention'].location}</p>
+                    <p>intervention_id: ${response['intervention'].intervention_id}</p>
+                    <p>status: ${response['intervention'].status}</p>
+                    <button type="submit" class="btn" value="edit">Edit status</button>
+                    </form>
+                </div>`;
+                console.log(output);         
+            
+            document.getElementById('popUp').innerHTML = output;
+            document.getElementById('popUp').style.display = 'block';
+        }
+    })
+}
