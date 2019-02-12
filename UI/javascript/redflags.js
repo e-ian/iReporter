23630c4263000781fce8cbf73c00eef6233d729b
e-ian <e-ian>
2019-02-12 12:11:47 +0300

    add js for getting a redflag by id

--- UI/javascript/redflags.js
+++ UI/javascript/redflags.js
@@ -39,3 +39,47 @@ function getRedflags() {
         });
 
 }
+
+// getting a specific redflag by id
+document.getElementById('getSpecific').onclick = function getSpecificredflag(){
+    let redflag_id = document.getElementById('incidentSearch').value;
+    console.log(redflag_id)
+    let specRedflagsURL = `http://127.0.0.1:5000/api/v1/redflags/${redflag_id}`;
+    console.log(specRedflagsURL)
+    token = localStorage.getItem('access_token')
+    fetch(specRedflagsURL, {
+        headers: {
+            'Content_type': 'application/json',
+            'Authorization': `${token}`
+        }
+    })    
+    .then(res => res.json())
+    .then(response => {
+        console.log(response)
+        if (response.message === 'Redflag not found'){
+            alert(response.message);} 
+        else{
+            let output = ''            
+                output += `
+                <div class="Create-record">
+                    <form id="IntForm" method="PUT">
+                    <h2>Redflag: ${redflag_id}</h2>
+                    <p>comment: ${response['redflag'].comment}</p>
+                    <p>created_by: ${response['redflag'].created_by}</p>
+                    <p>created_on: ${response['redflag'].created_on}</p>
+                    <p>image: ${response['redflag'].image}</p>
+                    <p>incident_type: ${response['redflag'].incident_type}</p>
+                    <p>location: ${response['redflag'].location}</p>
+                    <p>redflag_id: ${response['redflag'].redflag_id}</p>
+                    <p>status: ${response['redflag'].status}</p>
+                    <input type="text" id="status" placeholder="input status" class="login-input">
+                    <button type="submit" class="btn">submit</button>
+                    </form>
+                </div>`;
+                console.log(output);         
+            
+            document.getElementById('popUp').innerHTML = output;
+            document.getElementById('popUp').style.display = 'block';
+        }
+    })
+}
